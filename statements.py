6865139206f64371cae88549fd124772a73dd828
Noby Fujioka <n.fujioka@abacusmaths.info>
2018-12-06 06:09:39 +0000

    useful operators

--- statements.py
+++ statements.py
@@ -60,4 +60,66 @@ else:
     print('x is not smaller than 5')
 
 
+# range
+for num in range(0, 11, 2):
+    print(num)
+
+rangeToList = list(range(0, 11, 2))
+print(rangeToList)
+
+# enumerate
+word = 'abcde'
+for index, letter in enumerate(word):
+    print(index)
+    print(letter)
+    print('\n')
+
+# zip
+print('zip >>>>')
+mylist1 = [1, 2, 3, 4, 5, 6]
+mylist2 = ['a', 'b', 'c']
+mylist3 = [100, 200, 300]
+
+print(zip(mylist1, mylist2, mylist3))
+
+for item in zip(mylist1, mylist2, mylist3):
+    print(item)
+
+zipToList = list(zip(mylist1, mylist2))
+print(zipToList)
+
+# Check if something is in a string or array
+doesExist = 2 in [1, 2, 3]
+print(doesExist)
+doesExist = 'x' in [1, 2, 3]
+print(doesExist)
+doesExist = 'W' in 'Hello World!'
+print(doesExist)
+doesExist = 'myKey' in { 'myKey': 10 }
+print(doesExist)
+d = { 'myKey': 10 }
+doesExist = 'myKey' in d.keys()
+print(doesExist)
+myList = [10, 20, 30, 15, 8, 100, 7]
+
+# min/max
+print(min(myList))
+print(max(myList))
+
+# random library
+print('random >>>')
+from random import shuffle
+mylist = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+shuffle(mylist)
+print(mylist)
+
+from random import randint
+myInt = randint(0, 100)
+print(myInt)
+
+# input - accepts anything, and returns a string
+result = input('What is your name? ')
+print(f'Hello, {result}!')
+result = int(input('Enter a number here ::'))
+print(f'You entered >> {result}')
 
